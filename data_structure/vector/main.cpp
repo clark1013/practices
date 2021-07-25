8a7297cb5f495a78a5c702f9e9889401e6223182
wenjiazhi <wenjiazhi@zhihu.com>
2021-07-25 16:56:56 +0800

    use linux debug

--- data_structure/vector/main.cpp
+++ data_structure/vector/main.cpp
@@ -1,6 +1,13 @@
 #include "vector.h"
-#include <stdio.h>
 
 int main(int argc, char* argv[]){
-    printf("123");
+    Vector<int> V;
+
+    V.insert(0);
+    V.insert(1);
+    V.insert(2);
+    V.insert(3);
+    V.insert(4);
+
+    print(V);
 }
--- data_structure/vector/vector.h
+++ data_structure/vector/vector.h
@@ -1,3 +1,5 @@
+#include <cstdio>
+
 typedef int Rank;
 #define DEFAULT_CAPACITY 3
 
@@ -23,4 +25,62 @@ public:
     Vector(Vector<T> const& V) { copyFrom(V._elem, 0, V._size); }
     // 析构函数
     ~Vector() { delete [] _elem; }
+
+    Rank size() { return _size; }
+    int capacity() { return _capacity; }
+
+    T& operator[] (Rank r);
+    const T& operator[] (Rank r) const;
+
+    Rank insert(Rank r, T const& e);
+    Rank insert(T const& e) { return insert(_size, e); }
 };
+
+
+// 重载下标操作符
+template <typename T>
+T& Vector<T>::operator[] (Rank r) {
+    return _elem[r];
+}
+
+template <typename T>
+const T& Vector<T>::operator[] (Rank r) const {
+    return _elem[r];
+}
+
+template <typename T>
+void print(Vector<T> v) {
+    printf("size:%d, cap:%d\n", v.size(), v.capacity());
+}
+
+template <typename T>
+void Vector<T>::copyFrom(T const* A, Rank lo, Rank hi) {
+    _elem = new T[(hi - lo) * 2];
+    _size = 0;
+    while (lo < hi) {
+	_elem[_size++] = A[lo++];
+    }
+}
+
+template <typename T>
+void Vector<T>::expand() {
+    if(_size < _capacity) return;
+    if(_capacity < DEFAULT_CAPACITY) _capacity = DEFAULT_CAPACITY;
+    T* oldElem = _elem;
+    _elem = new T[ _capacity <<= 1 ];
+    for(int i=0; i < _size; i++) {
+	_elem[i] = oldElem[i];
+    }
+    delete [] oldElem;
+}
+
+template <typename T>
+Rank Vector<T>::insert(Rank r, T const& e) {
+    expand();
+    for (int i=_size; i > r; i--) {
+	_elem[i] = _elem[i-1];
+    }
+    _elem[r] = e;
+    _size++;
+    return r;
+}
