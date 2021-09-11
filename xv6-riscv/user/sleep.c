55c113faea799fa5ac21b0d30c0604520aacc963
clark1013 <clark1013@hotmail.com>
2021-09-11 16:45:25 +0800

    fix

---
+++ xv6-riscv/user/sleep.c
@@ -0,0 +1,17 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+void
+main(uint argc, char *argv[]) {
+	if (!(argc == 2)) {
+		printf("Usage: sleep [time]\n");
+		exit(1);
+	}
+	int t = atoi(argv[1]);
+	if (t <= 0) {
+		printf("Invalid time\n");
+		exit(1);
+	}
+	sleep(t);
+	exit(0);
+}
