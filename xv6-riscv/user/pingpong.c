8c380f7082c44d7d9afa612a81f78204a3146dee
Clark <clark1013@hotmail.com>
2021-10-25 23:32:59 +0800

    https://pdos.csail.mit.edu/6.S081/2020/labs/util.html pingpong

--- xv6-riscv/user/pingpong.c
+++ xv6-riscv/user/pingpong.c
@@ -1,24 +1,23 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-void main(int argc, char *arg[]) {
+void main(int argc, char *argv[]) {
   int p[2];
-  char *argv[2];
-
-  argv[0] = "wc";
-  argv[1] = 0;
 
   pipe(p);
   if (fork() == 0) {
     close(0);
     dup(p[0]);
-    close(p[0]);
     close(p[1]);
-    exec("wc", argv);
+    char child[4];
+    read(p[0], child, 4);
+    printf("%d: received %s\n", getpid(), child);
+    close(p[0]);
   } else {
     close(p[0]);
-    write(p[1], "hello world\n", 12);
+    write(p[1], "ping", 4);
     close(p[1]);
   }
+  wait(0);
   exit(0);
 }
