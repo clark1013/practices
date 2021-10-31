7c1fa9b776ea3e036a935def6ea2812d63ef8c73
Clark <clark1013@hotmail.com>
2021-10-31 14:18:18 +0800

    fix

--- xv6-riscv/user/pingpong.c
+++ xv6-riscv/user/pingpong.c
@@ -8,15 +8,21 @@ void main(int argc, char *argv[]) {
   if (fork() == 0) {
     close(0);
     dup(p[0]);
-    close(p[1]);
     char child[4];
     read(p[0], child, 4);
     printf("%d: received %s\n", getpid(), child);
+    write(p[1], "pong", 4);
+    close(p[1]);
     close(p[0]);
   } else {
-    close(p[0]);
+    close(0);
+    dup(p[0]);
     write(p[1], "ping", 4);
+    char child[4];
+    read(p[0], child, 4);
+    printf("%d: received %s\n", getpid(), child);
     close(p[1]);
+    close(p[0]);
   }
   wait(0);
   exit(0);
