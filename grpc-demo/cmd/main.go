7469ae777bcc605b253a44b71f74d762efce8877
Wen Jiazhi <jiazhi.wen@pingcap.com>
2022-07-13 23:16:11 +0800

    add grpc gateway error code demo

---
+++ grpc-demo/cmd/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"reflect"
+
+	pb "github.com/clark1013/grpc-demo/gen/go"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/pingcap/errcode"
+	grpcerr "github.com/pingcap/errcode/grpc"
+	"github.com/pingcap/log"
+	"github.com/tidbcloud/pkgv2/bizerr"
+	grpcbizerr "github.com/tidbcloud/pkgv2/grpc/middleware/bizerr"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+)
+
+type server struct {
+	pb.UnimplementedTestServiceServer
+}
+
+// MARK: 业务中返回 error
+func (s *server) Echo(ctx context.Context, t *pb.StringMessage) (*pb.TestResponse, error) {
+	// return t, nil
+	// return &pb.TestResponse{}, errcode.NewForbiddenErr(bizerr.New(bizerr.ErrCodeGeneralBusiness, "1234567"))
+	// return &pb.TestResponse{}, bizerr.New(bizerr.ErrCodeGeneralBusiness, "1234567")
+	// return nil, errcode.NewForbiddenErr(bizerr.New(bizerr.ErrCodeGeneralBusiness, "1234567"))
+	return nil, bizerr.New(bizerr.ErrCodeGeneralBusiness, "1234567")
+	// return nil, status.Error(4000321, "fhdkfdksfe")
+}
+
+func errorConvert(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	if coded := errcode.CodeChain(err); coded != nil {
+		return grpcerr.WrapAsGRPC(coded)
+	}
+	return err
+}
+
+func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		resp, err := handler(ctx, req)
+		return resp, errorConvert(err)
+	}
+}
+
+func main() {
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(
+			grpc_middleware.ChainUnaryServer(
+				// MARK: 解析 bizerr
+				grpcbizerr.UnaryServerInterceptor(func(resp interface{}, err bizerr.BizError) (resp_ interface{}, err_ error) {
+					// typed := getBaseRespFromResp(resp)
+					// if typed == nil {
+					// 	return resp, err
+					// }
+					// typed.ErrCode = int64(err.ErrCode())
+					// typed.ErrMsg = err.ErrMsg()
+					// return resp, nil
+					return resp, status.Error(codes.Code(err.ErrCode()), err.ErrMsg())
+				}),
+				UnaryServerInterceptor(),
+			),
+		),
+	)
+	pb.RegisterTestServiceServer(s, &server{})
+	addr := "0.0.0.0:9200"
+	go func() {
+		log.Info("serving gRPC", zap.String("address", addr))
+		lis, err := net.Listen("tcp", addr)
+		if err != nil {
+			panic(err)
+		}
+		err = s.Serve(lis)
+		if err != nil {
+			panic(err)
+		}
+	}()
+	// MARK: 自定义错误处理
+	mux := runtime.NewServeMux(
+		runtime.WithErrorHandler(func(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, writer http.ResponseWriter, request *http.Request, err error) {
+			newError := runtime.HTTPStatusError{
+				HTTPStatus: 400,
+				Err:        err,
+			}
+			runtime.DefaultHTTPErrorHandler(ctx, mux, marshaler, writer, request, &newError)
+		}),
+	)
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err := pb.RegisterTestServiceHandlerFromEndpoint(context.Background(), mux, addr, opts)
+	if err != nil {
+		panic("111")
+	}
+	http.ListenAndServe(":8081", mux)
+}
+
+// TODO(chenfei): merge with idl/pbgen/util/helper.go:GetBaseRespFromResp
+func getBaseRespFromResp(resp interface{}) *pb.BaseResp {
+	if resp == nil {
+		return nil
+	}
+
+	baseResp := reflect.ValueOf(resp).Elem().FieldByName("BaseResp")
+	if !baseResp.IsValid() { // don't have BaseReq field
+		return nil
+	}
+	if baseResp.IsNil() {
+		baseResp.Set(reflect.New(baseResp.Type().Elem()))
+	}
+
+	typed, ok := baseResp.Interface().(*pb.BaseResp)
+	if !ok {
+		return nil
+	}
+	return typed
+}
---
+++ grpc-demo/go.mod
@@ -0,0 +1,29 @@
+module github.com/clark1013/grpc-demo
+
+go 1.18
+
+require (
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.3
+	github.com/pingcap/errcode v0.3.0
+	github.com/pingcap/log v0.0.0-20210906054005-afc726e70354
+	github.com/tidbcloud/pkgv2 v0.3.4
+	go.uber.org/zap v1.19.1
+	google.golang.org/grpc v1.46.2
+	google.golang.org/protobuf v1.28.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.3 // indirect
+	github.com/pingcap/errors v0.11.4 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220519153652-3a47de7e79bd // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gorm.io/gorm v1.22.4 // indirect
+)
