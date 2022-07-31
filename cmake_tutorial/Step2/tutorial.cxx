57a186246e6ddfeadc06ef53be83c336eb37ca22
Wen Jiazhi <jiazhi.wen@pingcap.com>
2022-07-31 16:43:17 +0800

    Step 2

--- cmake_tutorial/Step2/tutorial.cxx
+++ cmake_tutorial/Step2/tutorial.cxx
@@ -4,6 +4,9 @@
 #include <string>
 
 #include "TutorialConfig.h"
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#endif
 
 int main(int argc, char* argv[])
 {
@@ -19,7 +22,13 @@ int main(int argc, char* argv[])
   const double inputValue = std::stod(argv[1]);
 
   // calculate square root
-  const double outputValue = sqrt(inputValue);
+  #ifdef USE_MYMATH
+    std::cout << "use my sqrt" << std::endl;
+    const double outputValue = mysqrt(inputValue);
+  #else
+    std::cout << "use std sqrt" << std::endl;
+    const double outputValue = sqrt(inputValue);
+  #endif
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
