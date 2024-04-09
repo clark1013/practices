7304ecf09298ff6707065878cc6d3663d2514b95
Wen Jiazhi <jiazhi.wen@pingcap.com>
2024-04-09 17:17:49 +0800

    finish project4b

--- talent-plan/tinykv/kv/server/server.go
+++ talent-plan/tinykv/kv/server/server.go
@@ -1,12 +1,14 @@
 package server
 
 import (
+	"bytes"
 	"context"
 
 	"github.com/pingcap-incubator/tinykv/kv/coprocessor"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/kv/storage/raft_storage"
 	"github.com/pingcap-incubator/tinykv/kv/transaction/latches"
+	"github.com/pingcap-incubator/tinykv/kv/transaction/mvcc"
 	coppb "github.com/pingcap-incubator/tinykv/proto/pkg/coprocessor"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/tinykvpb"
@@ -50,17 +52,156 @@ func (server *Server) Snapshot(stream tinykvpb.TinyKv_SnapshotServer) error {
 // Transactional API.
 func (server *Server) KvGet(_ context.Context, req *kvrpcpb.GetRequest) (*kvrpcpb.GetResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	reader, err := server.storage.Reader(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	txn := mvcc.NewMvccTxn(reader, req.Version)
+
+	lock, err := txn.GetLock(req.Key)
+	if err != nil {
+		return nil, err
+	}
+	if lock != nil && lock.Ts < req.Version {
+		return &kvrpcpb.GetResponse{
+			Error: &kvrpcpb.KeyError{
+				Locked: lock.Info(req.Key),
+			},
+		}, nil
+	}
+
+	value, err := txn.GetValue(req.Key)
+	if err != nil {
+		return nil, err
+	}
+
+	notfound := bytes.Equal(value, []byte{})
+	return &kvrpcpb.GetResponse{
+		Value:    value,
+		NotFound: notfound,
+	}, nil
 }
 
 func (server *Server) KvPrewrite(_ context.Context, req *kvrpcpb.PrewriteRequest) (*kvrpcpb.PrewriteResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	reader, err := server.storage.Reader(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	txn := mvcc.NewMvccTxn(reader, req.StartVersion)
+
+	keyErrs := make([]*kvrpcpb.KeyError, 0)
+	for _, mutation := range req.Mutations {
+		_, version, err := txn.MostRecentWrite(mutation.Key)
+		if err != nil {
+			return nil, err
+		}
+		if version > req.StartVersion {
+			keyErrs = append(keyErrs, &kvrpcpb.KeyError{
+				Conflict: &kvrpcpb.WriteConflict{
+					StartTs:    req.StartVersion,
+					ConflictTs: version,
+					Key:        mutation.Key,
+					Primary:    req.PrimaryLock,
+				},
+			})
+			continue
+		}
+		lock, err := txn.GetLock(mutation.Key)
+		if err != nil {
+			return nil, err
+		}
+		if lock != nil {
+			keyErrs = append(keyErrs, &kvrpcpb.KeyError{
+				Locked: lock.Info(mutation.Key),
+			})
+			continue
+		}
+
+		switch mutation.Op {
+		case kvrpcpb.Op_Put:
+			txn.PutValue(mutation.Key, mutation.Value)
+			txn.PutLock(mutation.Key, &mvcc.Lock{
+				Primary: req.PrimaryLock,
+				Ts:      req.StartVersion,
+				Ttl:     req.LockTtl,
+				Kind:    mvcc.WriteKindPut,
+			})
+		case kvrpcpb.Op_Del:
+			txn.DeleteValue(mutation.Key)
+			txn.PutLock(mutation.Key, &mvcc.Lock{
+				Primary: req.PrimaryLock,
+				Ts:      req.StartVersion,
+				Ttl:     req.LockTtl,
+				Kind:    mvcc.WriteKindDelete,
+			})
+		}
+	}
+	if len(keyErrs) > 0 {
+		return &kvrpcpb.PrewriteResponse{
+			Errors: keyErrs,
+		}, nil
+	}
+
+	if err := server.storage.Write(req.Context, txn.Writes()); err != nil {
+		return nil, err
+	}
+
+	return &kvrpcpb.PrewriteResponse{}, nil
 }
 
 func (server *Server) KvCommit(_ context.Context, req *kvrpcpb.CommitRequest) (*kvrpcpb.CommitResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	reader, err := server.storage.Reader(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	txn := mvcc.NewMvccTxn(reader, req.StartVersion)
+
+	for _, key := range req.Keys {
+		lock, err := txn.GetLock(key)
+		if err != nil {
+			return nil, err
+		}
+		if lock == nil {
+			write, _, err := txn.CurrentWrite(key)
+			if err != nil {
+				return nil, err
+			}
+			if write != nil && write.StartTS == req.StartVersion && write.Kind == mvcc.WriteKindRollback {
+				return &kvrpcpb.CommitResponse{
+					Error: &kvrpcpb.KeyError{Retryable: "true"},
+				}, nil
+			}
+			continue
+		} else if lock.Ts != req.StartVersion {
+			return &kvrpcpb.CommitResponse{
+				Error: &kvrpcpb.KeyError{Retryable: "true"},
+			}, nil
+		}
+		txn.PutWrite(key, req.CommitVersion, &mvcc.Write{
+			StartTS: lock.Ts,
+			Kind:    lock.Kind,
+		})
+	}
+
+	if err := server.storage.Write(req.Context, txn.Writes()); err != nil {
+		return nil, err
+	}
+
+	removeLockTxn := mvcc.NewMvccTxn(reader, req.StartVersion)
+	for _, key := range req.Keys {
+		removeLockTxn.DeleteLock(key)
+	}
+
+	if err := server.storage.Write(req.Context, removeLockTxn.Writes()); err != nil {
+		return nil, err
+	}
+
+	return &kvrpcpb.CommitResponse{}, nil
 }
 
 func (server *Server) KvScan(_ context.Context, req *kvrpcpb.ScanRequest) (*kvrpcpb.ScanResponse, error) {
--- talent-plan/tinykv/kv/transaction/mvcc/transaction.go
+++ talent-plan/tinykv/kv/transaction/mvcc/transaction.go
@@ -1,10 +1,12 @@
 package mvcc
 
 import (
+	"bytes"
 	"encoding/binary"
 
 	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/kv/util/codec"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 	"github.com/pingcap-incubator/tinykv/scheduler/pkg/tsoutil"
 )
@@ -41,46 +43,149 @@ func (txn *MvccTxn) Writes() []storage.Modify {
 // PutWrite records a write at key and ts.
 func (txn *MvccTxn) PutWrite(key []byte, ts uint64, write *Write) {
 	// Your Code Here (4A).
+	data := write.ToBytes()
+	txn.writes = append(txn.writes, storage.Modify{
+		Data: storage.Put{
+			Key:   EncodeKey(key, ts),
+			Value: data,
+			Cf:    engine_util.CfWrite,
+		},
+	})
 }
 
 // GetLock returns a lock if key is locked. It will return (nil, nil) if there is no lock on key, and (nil, err)
 // if an error occurs during lookup.
 func (txn *MvccTxn) GetLock(key []byte) (*Lock, error) {
 	// Your Code Here (4A).
-	return nil, nil
+	data, err := txn.Reader.GetCF(engine_util.CfLock, key)
+	if err != nil {
+		return nil, err
+	}
+	if data == nil {
+		return nil, nil
+	}
+	return ParseLock(data)
 }
 
 // PutLock adds a key/lock to this transaction.
 func (txn *MvccTxn) PutLock(key []byte, lock *Lock) {
 	// Your Code Here (4A).
+	data := lock.ToBytes()
+	txn.writes = append(txn.writes, storage.Modify{
+		Data: storage.Put{
+			Key:   key,
+			Value: data,
+			Cf:    engine_util.CfLock,
+		},
+	})
 }
 
 // DeleteLock adds a delete lock to this transaction.
 func (txn *MvccTxn) DeleteLock(key []byte) {
 	// Your Code Here (4A).
+	txn.writes = append(txn.writes, storage.Modify{
+		Data: storage.Delete{
+			Key: key,
+			Cf:  engine_util.CfLock,
+		},
+	})
 }
 
 // GetValue finds the value for key, valid at the start timestamp of this transaction.
 // I.e., the most recent value committed before the start of this transaction.
 func (txn *MvccTxn) GetValue(key []byte) ([]byte, error) {
 	// Your Code Here (4A).
+	iter := txn.Reader.IterCF(engine_util.CfWrite)
+	defer iter.Close()
+
+	iter.Seek(key)
+	for iter.Valid() {
+		item := iter.Item()
+		itemKey := item.Key()
+		value, err := item.Value()
+		if err != nil {
+			return nil, err
+		}
+
+		userKey := DecodeUserKey(itemKey)
+		ts := decodeTimestamp(itemKey)
+		if !bytes.Equal(userKey, key) {
+			break
+		}
+
+		write, err := ParseWrite(value)
+		if err != nil {
+			return nil, err
+		}
+
+		if ts <= txn.StartTS {
+			return txn.Reader.GetCF(
+				engine_util.CfDefault,
+				EncodeKey(key, write.StartTS),
+			)
+		}
+
+		iter.Next()
+	}
 	return nil, nil
 }
 
 // PutValue adds a key/value write to this transaction.
 func (txn *MvccTxn) PutValue(key []byte, value []byte) {
 	// Your Code Here (4A).
+	txn.writes = append(txn.writes, storage.Modify{
+		Data: storage.Put{
+			Key:   EncodeKey(key, txn.StartTS),
+			Value: value,
+			Cf:    engine_util.CfDefault,
+		},
+	})
 }
 
 // DeleteValue removes a key/value pair in this transaction.
 func (txn *MvccTxn) DeleteValue(key []byte) {
 	// Your Code Here (4A).
+	txn.writes = append(txn.writes, storage.Modify{
+		Data: storage.Delete{
+			Key: EncodeKey(key, txn.StartTS),
+			Cf:  engine_util.CfDefault,
+		},
+	})
 }
 
 // CurrentWrite searches for a write with this transaction's start timestamp. It returns a Write from the DB and that
 // write's commit timestamp, or an error.
 func (txn *MvccTxn) CurrentWrite(key []byte) (*Write, uint64, error) {
 	// Your Code Here (4A).
+	iter := txn.Reader.IterCF(engine_util.CfWrite)
+	defer iter.Close()
+
+	iter.Seek(key)
+	for iter.Valid() {
+		item := iter.Item()
+		itemKey := item.Key()
+		value, err := item.Value()
+		if err != nil {
+			return nil, 0, err
+		}
+
+		userKey := DecodeUserKey(itemKey)
+		ts := decodeTimestamp(itemKey)
+		if !bytes.Equal(userKey, key) {
+			break
+		}
+
+		write, err := ParseWrite(value)
+		if err != nil {
+			return nil, 0, err
+		}
+		if write.StartTS == txn.StartTS {
+			return write, ts, nil
+		}
+
+		iter.Next()
+	}
+
 	return nil, 0, nil
 }
 
@@ -88,6 +193,32 @@ func (txn *MvccTxn) CurrentWrite(key []byte) (*Write, uint64, error) {
 // write's commit timestamp, or an error.
 func (txn *MvccTxn) MostRecentWrite(key []byte) (*Write, uint64, error) {
 	// Your Code Here (4A).
+	iter := txn.Reader.IterCF(engine_util.CfWrite)
+	defer iter.Close()
+
+	iter.Seek(key)
+	if iter.Valid() {
+		item := iter.Item()
+		itemKey := item.Key()
+		value, err := item.Value()
+		if err != nil {
+			return nil, 0, err
+		}
+
+		userKey := DecodeUserKey(itemKey)
+		ts := decodeTimestamp(itemKey)
+		if !bytes.Equal(userKey, key) {
+			return nil, 0, nil
+		}
+
+		write, err := ParseWrite(value)
+		if err != nil {
+			return nil, 0, err
+		}
+
+		return write, ts, nil
+	}
+
 	return nil, 0, nil
 }
 
